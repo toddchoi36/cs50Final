6cb9e707d7e7df30006449f360a1d600af7c5e26
Todd Choi <toddchoi36@gmail.com>
2020-09-16 16:14:01 -0400

    [

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ Session(app)
 engine = create_engine("")
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/", methods=["GET")
+@app.route("/", methods=["GET"])
 @login_required
 def index():
     """Show portfolio of stocks"""
