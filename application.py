855a1bef67f755dd7d68c2b0c4de3675449fbee5
Todd Choi <toddchoi36@gmail.com>
2020-09-30 17:35:37 -0400

    uhm

--- application.py
+++ application.py
@@ -56,7 +56,7 @@ def importcsv():
     else:
         #sales_file = request.files['csv']
         #sales_file.save(os.path.join(app.config["SALES_DATA"], sales_file.filename))
-        f = open("flights.csv")
+        f = open("sample.csv")
         read = csv.reader(f)
         date = request.form.get("date")
 
