a5e07cebc68bb05be8dfbcc5ab4ab34be5fa3b67
Todd Choi <toddchoi36@gmail.com>
2020-10-01 15:12:46 -0400

    hope

--- application.py
+++ application.py
@@ -155,10 +155,11 @@ def register():
             db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {"username": username, "hash": password})
             db.commit()
             
-            user = db.execute("SELECT id FROM users WHERE username =:username", {"username": username}).fetchone()
+            user = db.execute("SELECT * FROM users WHERE username =:username", {"username": username})
             db.commit
-            sessionid = (''.join(map(str, user)))
-            session["user_id"] = sessionid
+            for r in users:
+                sessionid = r[0]
+                session["user_id"] = sessionid
             
             return redirect("/")
         else:
