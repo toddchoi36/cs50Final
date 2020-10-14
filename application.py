5f2152bbab4f810afb99d199e2ca96c780a9aeb6
Todd Choi <toddchoi36@gmail.com>
2020-10-14 16:21:54 -0400

    ugh

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_uploads import UploadSet, configure_uploads
+
 
 from helpers import apology, login_required, usd
 
